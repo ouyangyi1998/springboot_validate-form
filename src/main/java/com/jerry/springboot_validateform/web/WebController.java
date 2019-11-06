11a1e4c3559f5bf72f3eaee8aef7553076506fec
ouyangyi1998 <1347401764@qq.com>
2019-11-06 16:30:36 +0800

    webMvcConfig

--- src/main/java/com/jerry/springboot_validateform/web/WebController.java
+++ src/main/java/com/jerry/springboot_validateform/web/WebController.java
@@ -6,12 +6,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
 
 @Controller
-public class WebController extends WebMvcConfigurerAdapter {
+public class WebController implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/results").setViewName("results");
